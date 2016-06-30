31cee06cb51e9e4765f903c04799d00727b5c5c5
hhy-ccj <772958417@qq.com>
2016-06-30 21:07:44 +0800

    install.sh加入获取当前文件夹相对路径

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
-mv $HOME/.vim 		$HOME/.vim_backup
-mv $HOME/.vimrc 	$HOME/.vimrc_backup
-mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh_backup
-mv $HOME/.zshrc 	$HOME/.zshrc_backup
-mv $HOME/.tmux.conf $HOME/.tmux.conf_backup
-ln -s LINUX_TOOL/vim/.vim		$HOME/
-ln -s LINUX_TOOL/vim/.vimrc		$HOME/
-ln -s LINUX_TOOL/zsh/.oh-my-zsh	$HOME/
-ln -s LINUX_TOOL/zsh/.zshrc		$HOME/
-ln -s LINUX_TOOL/zsh/.tmux.conf	$HOME/
+# 为原来文件备份
+mv $HOME/.vim						$HOME/.vim_backup
+mv $HOME/.vimrc						$HOME/.vimrc_backup
+mv $HOME/.oh-my-zsh					$HOME/.oh-my-zsh_backup
+mv $HOME/.zshrc						$HOME/.zshrc_backup
+mv $HOME/.tmux.conf					$HOME/.tmux.conf_backup
+# 在$HOME下创建快捷方式
+tool_path=$(cd "$(dirname "$0")"; pwd)
+ln -s $tool_path/vim/.vim			$HOME/
+ln -s $tool_path/vim/.vimrc			$HOME/
+ln -s $tool_path/zsh/.oh-my-zsh		$HOME/
+ln -s $tool_path/zsh/.zshrc			$HOME/
+ln -s $tool_path/zsh/.tmux.conf		$HOME/
