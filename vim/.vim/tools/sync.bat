746db4fb5e45ba23059845277f196b726b7b821a
hhy-ccj <772958417@qq.com>
2016-07-19 15:30:23 +0800

    add vim windows sync.bat
    add vim windows sync.bat

---
+++ vim/.vim/tools/sync.bat
@@ -0,0 +1,5 @@
+del tags cscope.out filenametags
+ctags -R --fields=+lS --languages=+Asm
+cscope -bR
+echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/">filenametags
+rem find . -not -regex '.*\.\(png\|gif\|o\|d\|obj\)'  ! -path "*git*" -type f -printf "%f\t%p\t1\n" | sort -f>>filenametags
