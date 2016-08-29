305386db84f31938763eb6a511990d6813bd8a60
hhy-ccj <772958417@qq.com>
2016-08-29 12:05:16 +0800

    vim sync.sh使用变量简化流程

--- vim/.vim/tools/sync.sh
+++ vim/.vim/tools/sync.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-filter_path="! -path "*br17*" ! -path "*ac461x_uboot_lib*" ! -path "*bt16*""
+filter_path="find . ! -path ".*br17*" ! -path ".*ac461x_uboot_lib*" ! -path ".*bt16*""
+# echo $filter_path
 rm tags cscope.out filenametags
 ctags -R --fields=+lS --languages=+Asm --c++-kinds=+p --fields=+iaS --extra=+q
 # ctags -R --c++-kinds=+p --fields=+liaS --extra=+q .
 # cscope -bR
 # find ! -path "*br17*" ! -path "*ac461x_uboot_lib*" ! -path "*bt16*" -regex '.*\.\(c\|h\)' > cscope.input
-find $filter_path -regex '.*\.\(c\|h\)' > cscope.input
+$filter_path -regex '.*\.\(c\|h\)' > cscope.input
 cscope -b -i cscope.input
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/">filenametags
 # find . -not -regex '.*\.\(png\|gif\|o\|d\|obj\|swp\)' ! -path "*br17*" ! -path "*ac461x_uboot_lib*" ! -path "*bt16*" ! -path "*git*" -type f -printf "%f\t%p\t1\n" | sort -f>>filenametags
-find . -not -regex '.*\.\(png\|gif\|o\|d\|obj\|swp\)' $filter_path ! -path "*git*" -type f -printf "%f\t%p\t1\n" | sort -f>>filenametags
+$filter_path -not -regex '.*\.\(png\|gif\|o\|d\|obj\|swp\)' ! -path "*git*" -type f -printf "%f\t%p\t1\n" | sort -f>>filenametags
