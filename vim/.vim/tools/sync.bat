eca16d788c88b1c76e10bf85f65cac435b5ed975
hhy-ccj <772958417@qq.com>
2016-11-23 13:42:17 +0800

    add easymotion & delete unused tool
    easymotion是一块光标快速定位插件，方便跳转到某个关键字

--- vim/.vim/tools/sync.bat
+++ vim/.vim/tools/sync.bat
@@ -1,5 +1,14 @@
+@echo off
 del tags cscope.out filenametags
 ctags -R --fields=+lS --languages=+Asm
 cscope -bR
 ::echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/">filenametags
-find_linux . -not -regex ".*\.\(doc\|pdf\|o\|d\|obj\|lst\)" ! -path "*git*" -type f -printf "%1" | sort_linux -f>>filenametags
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+   %~dp0\win32\find_linux32 . -not -regex ".*\.\(doc\|pdf\|o\|d\|obj\|lst\)" ^
+   ! -path "*git*" ^
+   ! -path "*bt16*" ^
+   -type f -printf "%1" | %~dp0\win32\sort_linux32 -f>>filenametags
+)
+if "%PROCESSOR_ARCHITECTURE%"=="x64" (
+   %~dp0\win64\find_linux64 . -not -regex ".*\.\(doc\|pdf\|o\|d\|obj\|lst\)" ! -path "*git*" ! -path "*bt16*" -type f -printf "%1" | %~dp0\win64\sort_linux64 -f>>filenametags
+)
